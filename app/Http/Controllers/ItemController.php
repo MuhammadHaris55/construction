f71e8c94e4deaa11e4c6116c49a549f0a0f6e734
MuhammadHaris55 <hkhn567@gmail.com>
2022-01-11 16:41:58 +0500

    data passing

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -9,136 +9,136 @@ use App\Models\Item;
 
 class ItemController extends Controller
 {
-    // public function index()
-    // {
-    //     //Validating request
-    //     request()->validate([
-    //         'direction' => ['in:asc,desc'],
-    //         'field' => ['in:name']
-    //     ]);
+    public function index()
+    {
+        //Validating request
+        request()->validate([
+            'direction' => ['in:asc,desc'],
+            'field' => ['in:name']
+        ]);
 
-    //     //Searching request
-    //     $query = Item::query();
+        //Searching request
+        $query = Item::query();
 
-    //     if (request('search')) {
-    //         $query->where('name', 'LIKE', '%' . request('search') . '%');
-    //     }
-
-
-    //     if (request('searche')) {
-    //         $query->where('hscode', 'LIKE', '%' . request('searche') . '%');
-    //     }
-
-    //     if (request()->has(['field', 'direction'])) {
-    //         $query->orderBy(request('field'), request('direction'));
-    //     } else {
-    //         $query->orderBy(('name'), ('asc'));
-    //     }
+        if (request('search')) {
+            $query->where('name', 'LIKE', '%' . request('search') . '%');
+        }
 
 
-    //     return Inertia::render('Items/Index', [
+        if (request('searche')) {
+            $query->where('hscode', 'LIKE', '%' . request('searche') . '%');
+        }
 
-    //         'filters' => request()->all(['search', 'searche', 'field', 'direction']),
-    //         'balances' => $query->paginate(10)
-    //             ->through(function ($item) {
-    //                 return [
-    //                     'id' => $item->id,
-    //                     'name' => $item->name,
-    //                     'description' => $item->description,
-    //                     'hscode' => $item->hscode,
-    //                     'unit_id' => $item->unitTypes->name,
-    //                     'delete' => Quantity::where('item_id', $item->id)->first() ? false : true,
-
-    //                 ];
-    //             }),
+        if (request()->has(['field', 'direction'])) {
+            $query->orderBy(request('field'), request('direction'));
+        } else {
+            $query->orderBy(('name'), ('asc'));
+        }
 
 
+        return Inertia::render('Items/Index', [
 
-    //         'companies' => Company::all()
-    //             ->map(
-    //                 function ($com) {
-    //                     return [
-    //                         'id' => $com->id,
-    //                         'name' => $com->name,
-    //                     ];
-    //                 }
-    //             ),
+            'filters' => request()->all(['search', 'searche', 'field', 'direction']),
+            'balances' => $query->paginate(10)
+                ->through(function ($item) {
+                    return [
+                        'id' => $item->id,
+                        'name' => $item->name,
+                        'description' => $item->description,
+                        'hscode' => $item->hscode,
+                        'unit_id' => $item->unitTypes->name,
+                        'delete' => Quantity::where('item_id', $item->id)->first() ? false : true,
 
-    //     ]);
-    // }
-
-    // public function create()
-    // {
-    //     $unittype = \App\Models\UnitType::all()->map->only('id', 'name');
-
-    //     if ($unittype->first()) {
-
-    //         return Inertia::render('Items/Create', [
-    //             'unittypes' => $unittype,
-    //         ]);
-    //     } else {
-    //         return Redirect::route('unittypes.create')->with('warning', 'UNIT TYPE NOT FOUND, Please create unit type first.');
-    //     }
-    // }
-
-    // public function store(Req  $request)
-    // {
-    //     Request::validate([
-
-    //         'items.*.name' => ['required', 'unique:items', 'max:255'],
-    //         'items.*.hscode' => ['required', 'unique:items', 'max:255'],
-    //     ]);
-
-    //     $items = $request->items;
-    //     // dd($accounts);
-    //     foreach ($items as $item) {
-    //         // dd($acc);
-    //         Item::create([
-    //             'name' => $item['name'],
-    //             'description' => $item['description'],
-    //             'hscode' => $item['hscode'],
-    //             'unit_id' => $item['unit_id'],
-    //             'file_id' => null,
+                    ];
+                }),
 
 
-    //         ]);
-    //     }
-    //     return Redirect::route('items')->with('success', 'Item created.');
-    // }
 
-    // public function edit(Item $item)
-    // {
-    //     return Inertia::render('Items/Edit', [
-    //         'item' => [
-    //             'id' => $item->id,
-    //             'name' => $item->name,
-    //             'description' => $item->description,
-    //             'hscode' => $item->hscode,
-    //             // 'unit_id' =>$item->unit_id,
+            'companies' => Company::all()
+                ->map(
+                    function ($com) {
+                        return [
+                            'id' => $com->id,
+                            'name' => $com->name,
+                        ];
+                    }
+                ),
 
-    //         ],
-    //     ]);
-    // }
+        ]);
+    }
 
-    // public function update(Item $item)
-    // {
-    //     Request::validate([
-    //         'name' => ['required', 'max:255'],
-    //         'hscode' => ['required', 'max:255'],
+    public function create()
+    {
+        $unittype = \App\Models\UnitType::all()->map->only('id', 'name');
 
-    //     ]);
+        if ($unittype->first()) {
 
-    //     $item->name = Request::input('name');
-    //     $item->description = Request::input('description');
-    //     $item->hscode = Request::input('hscode');
-    //     $item->save();
+            return Inertia::render('Items/Create', [
+                'unittypes' => $unittype,
+            ]);
+        } else {
+            return Redirect::route('unittypes.create')->with('warning', 'UNIT TYPE NOT FOUND, Please create unit type first.');
+        }
+    }
 
-    //     return Redirect::route('items')->with('success', 'Item updated.');
-    // }
+    public function store(Req  $request)
+    {
+        Request::validate([
 
-    // public function destroy(Item $item)
-    // {
-    //     $item->delete();
-    //     return Redirect::back()->with('success', 'Item deleted.');
-    // }
+            'items.*.name' => ['required', 'unique:items', 'max:255'],
+            'items.*.hscode' => ['required', 'unique:items', 'max:255'],
+        ]);
+
+        $items = $request->items;
+        // dd($accounts);
+        foreach ($items as $item) {
+            // dd($acc);
+            Item::create([
+                'name' => $item['name'],
+                'description' => $item['description'],
+                'hscode' => $item['hscode'],
+                'unit_id' => $item['unit_id'],
+                'file_id' => null,
+
+
+            ]);
+        }
+        return Redirect::route('items')->with('success', 'Item created.');
+    }
+
+    public function edit(Item $item)
+    {
+        return Inertia::render('Items/Edit', [
+            'item' => [
+                'id' => $item->id,
+                'name' => $item->name,
+                'description' => $item->description,
+                'hscode' => $item->hscode,
+                // 'unit_id' =>$item->unit_id,
+
+            ],
+        ]);
+    }
+
+    public function update(Item $item)
+    {
+        Request::validate([
+            'name' => ['required', 'max:255'],
+            'hscode' => ['required', 'max:255'],
+
+        ]);
+
+        $item->name = Request::input('name');
+        $item->description = Request::input('description');
+        $item->hscode = Request::input('hscode');
+        $item->save();
+
+        return Redirect::route('items')->with('success', 'Item updated.');
+    }
+
+    public function destroy(Item $item)
+    {
+        $item->delete();
+        return Redirect::back()->with('success', 'Item deleted.');
+    }
 }
--- app/Http/Controllers/TradeController.php
+++ app/Http/Controllers/TradeController.php
@@ -9,5 +9,94 @@ use App\Models\Trade;
 
 class TradeController extends Controller
 {
-    //
+    public function index()
+    {
+        //Validating request
+        request()->validate([
+            'direction' => ['in:asc,desc'],
+            'field' => ['in:name']
+        ]);
+
+        $query = Trade::query();
+
+        //Searching request
+        if (request('search')) {
+            $query->where('name', 'LIKE', '%' . request('search') . '%');
+        }
+        //Ordering request
+        if (request()->has(['field', 'direction'])) {
+            $query->orderBy(
+                request('field'),
+                request('direction')
+            );
+        }
+
+        return Inertia::render('Trades/Index', [
+            'trade' => Trade::first(),
+            'balances' => $query->paginate(12)
+            ->through(
+                fn ($trade) =>
+                [
+                    'id' => $trade->id,
+                    'name' => $trade->name,
+                    'start' => $trade->start,
+                    'end' => $trade->end,
+                    'revenue' => $trade->revenue,
+                    'cost' => $trade->cost,
+                    'actual' => $trade->actual,
+                    'project_id' => $trade->project_id,
+                    'enable' => $trade->enable,
+                    // 'delete' => Item::where('trade_id', $trade->id)->first() ? false : true,
+                ],
+            ),
+            'filters' => request()->all(['search', 'field', 'direction'])
+        ]);
+    }
+
+    public function create()
+    {
+        return Inertia::render('Trades/Create');
+    }
+
+    public function store()
+    {
+        Request::validate([
+            'name' => ['required', 'unique:agents', 'max:255'],
+
+        ]);
+        $trade = Trade::create([
+            'name' => strtoupper(Request::input('name')),
+        ]);
+
+        return Redirect::route('agents')->with('success', 'Trade created');
+    }
+
+    public function edit(Trade $trade)
+    {
+        return Inertia::render('Trades/Edit', [
+            'trade' => [
+                'id' => $trade->id,
+                'name' => $trade->name,
+            ],
+        ]);
+    }
+
+    public function update(Trade $trade)
+    {
+        Request::validate([
+            'name' => ['required', 'max:255'],
+        ]);
+
+        $trade->name = strtoupper(Request::input('name'));
+
+        $trade->save();
+
+        return Redirect::route('agents')->with('success', 'Trade updated.');
+    }
+
+    public function destroy(Trade $trade)
+    {
+        $trade->delete();
+        return Redirect::back()->with('success', 'Trade deleted.');
+    }
 }
